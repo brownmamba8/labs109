b5066ce15c1c0e3b361c8221ed51091baa73ee55
Kallu609 <khirvola@gmail.com>
2018-10-08 15:40:07 +0300

    Solving

--- labs109.py
+++ labs109.py
@@ -1,17 +1,17 @@
-def ryerson_letter_grade(pct):
-    """
-    TODO
-    """
-    if 0 < pct < 50:
-        return 'F'
-    elif pct <= 59:
-        return 'D'
-    elif pct <= 69:
-        return 'C'
-    elif pct <= 79:
-        return 'B'
-    elif pct <= 100:
-        return 'A'
+# def ryerson_letter_grade(pct):
+#     """
+#     TODO
+#     """
+#     if 0 < pct < 50:
+#         return 'F'
+#     elif pct <= 59:
+#         return 'D'
+#     elif pct <= 69:
+#         return 'C'
+#     elif pct <= 79:
+#         return 'B'
+#     elif pct <= 100:
+#         return 'A'
 
 
 def is_ascending(items):
@@ -36,21 +36,21 @@ def double_until_all_digits(n, giveup=1000):
     return -1
 
 
-def caps_lock_stuck(text):
-    """
-    Should work but doesn't pass
-    """
-    caps_lock = False
-    new_text = ''
-
-    for char in text:
-        if char.lower() in ['a', 'z']:
-            caps_lock = not caps_lock
-            continue
-
-        new_text += char.swapcase() if caps_lock else char
-
-    return new_text
+# def caps_lock_stuck(text):
+#     """
+#     TODO Should work but doesn't pass
+#     """
+#     caps_lock = False
+#     new_text = ''
+#
+#     for char in text:
+#         if char.lower() in ['a', 'z']:
+#             caps_lock = not caps_lock
+#             continue
+#
+#         new_text += char.swapcase() if caps_lock else char
+#
+#     return new_text
 
 
 def scrabble_value(word, multipliers):
@@ -85,49 +85,100 @@ def create_zigzag(rows, cols, start=1):
     return grid
 
 
-def contains_bingo(card, numbers, centerfree=True):
-    rows = []
-
-    for x in range(5):
-        # Diagonals
-        h_row = []
-        v_row = []
-
-        for y in range(5):
-            if x == y == 2 and centerfree:
-                continue
-
-            h_row.append(card[x][y])
-            v_row.append(card[y][x])
-
-    # Diagonals
-    for i in range(5):
-
-
-    for row in rows:
-        if set(row).issubset(numbers):
-            return True
-
-    return False
-
-
-c = [
-    [38, 93, 42, 47, 15],
-    [90, 13, 41, 10, 56],
-    [54, 23, 87, 70, 6],
-    [86, 43, 48, 40, 92],
-    [71, 24, 44, 1, 34]
-]
-
-n = [
-    1, 2, 3, 4, 6, 8, 12,
-    13, 15, 16, 19, 21, 22,
-    24, 28, 34, 38, 40, 41,
-    42, 43, 45, 47, 49, 51,
-    53, 55, 57, 58, 62, 65,
-    66, 69, 70, 72, 82, 83,
-    84, 86, 88, 95, 97
-]
+# def contains_bingo(card, numbers, centerfree=True):
+#     """
+#     TODO Test fails for some reason
+#     """
+#     rows = []
+#     size = 5  # Should be => size % 2 == 1
+#
+#     # Horizontal & Vertical
+#     for x in range(size):
+#         h_row = []
+#         v_row = []
+#
+#         for y in range(size):
+#             if x == y == ((size - 1) / 2) and centerfree:
+#                 continue
+#
+#             h_row.append(card[x][y])
+#             v_row.append(card[y][x])
+#
+#         rows.append(h_row)
+#         rows.append(v_row)
+#
+#     # Diagonals
+#     d_rows = [
+#         [card[size - i - 1][i] for i in range(size)],
+#         [card[i][i] for i in range(size)]
+#     ]
+#
+#     for row in d_rows:
+#         if centerfree:
+#             del row[(size - 1) / 2]
+#
+#     rows += d_rows
+#
+#     for row in rows:
+#         if set(row).issubset(numbers):
+#             return True
+#
+#     return False
 
 
-print(contains_bingo(c, n))
+def group_equal(items):
+    groups, group = [], []
+
+    for i, item in enumerate(items):
+        group.append(item)
+
+        if i + 1 >= len(items):
+            groups.append(group)
+            continue
+
+        next_item = items[i + 1]
+
+        if next_item == item:
+            continue
+
+        groups.append(group)
+        group = []
+
+    return groups
+
+
+# def recaman(n):
+#     """
+#     TODO
+#     """
+#     pass
+#
+#
+# def running_median_of_three(items):
+#     """
+#     TODO
+#     """
+#     pass
+
+
+def detab(text, n=8, sub=' '):
+    while True:
+        tab_index = text.find('\t')
+
+        if tab_index == -1:
+            break
+
+        text = text.replace('\t', (n - n % tab_index) * sub, 1)
+
+    return text
+
+
+def reverse_ascending_sublists(items):
+    cur = 0
+
+    for i, item in enumerate(items):
+        if i + 1 >= len(items) or item >= items[i + 1]:
+            items[cur:i + 1] = items[cur:i + 1][::-1]
+            cur = i + 1
+
+    return items
---
+++ test.py
@@ -0,0 +1,3 @@
+from labs109 import reverse_ascending_sublists
+
+print(reverse_ascending_sublists([1,2,2,3]))
